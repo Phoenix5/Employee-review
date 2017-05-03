e26c07cc4be46769b142e35361da3939c5883dd8
Phoenix5 <mashelkarprathamesh@gmail.com>
2017-05-03 19:31:58 +0530

    Api-load

---
+++ ConsoleApi1/ConsoleTest/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleTest
+{
+        using System;
+        using System.Net.Http;
+
+        class Program
+        {
+            private static readonly int _connections = 10;
+            private static readonly HttpClient _httpClient = new HttpClient();
+
+            private static void Main()
+            {
+                TestHttpClientWithStaticInstance();
+                TestHttpClientWithUsing();
+            Console.ReadLine();
+            }
+
+            private static void TestHttpClientWithUsing()
+            {
+                try
+                {
+                    for (var i = 0; i < _connections; i++)
+                    {
+                        using (var httpClient = new HttpClient())
+                        {
+                            var result = httpClient.GetAsync(new Uri("http://bing.com")).Result;
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                }
+            }
+
+            private static void TestHttpClientWithStaticInstance()
+            {
+                try
+                {
+                    for (var i = 0; i < _connections; i++)
+                    {
+                        var result = _httpClient.GetAsync(new Uri("http://bing.com")).Result;
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                }
+            }
+        }
+    }
+
